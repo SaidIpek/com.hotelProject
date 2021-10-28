cd66dfb0d2b8749671468f2c1f92c10b712c2492
Arslnmrv <arslanmerve1619@gmail.com>
2021-10-28 11:57:37 +0300

    updated

---
+++ src/test/java/pages/QAConcortHotelPage.java
@@ -0,0 +1,44 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class QAConcortHotelPage {
+
+    WebDriver driver;
+
+    public QAConcortHotelPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+   // @FindBy(linkText = "Log in")
+    @FindBy (xpath = "(//a[@class='nav-link'])[7]")
+    public WebElement ilkLoginLinki;
+
+    @FindBy(id = "UserName")
+    public WebElement usernameKutusu;
+
+    @FindBy(id = "Password")
+    public WebElement passwordKutusu;
+
+    @FindBy(id = "btnSubmit")
+    public WebElement loginButonu;
+
+    public void ConcortHotelLogin() throws InterruptedException {
+        Driver.getDriver().get(ConfigReader.getProperty("CHQAUrl"));
+        QAConcortHotelPage qaConcortHotelPage = new QAConcortHotelPage();
+        Thread.sleep(1000);
+        qaConcortHotelPage.ilkLoginLinki.click();
+        Thread.sleep(2000);
+        qaConcortHotelPage.usernameKutusu.sendKeys(ConfigReader.getProperty("CHQAValidUsername"));
+        Thread.sleep(1000);
+        qaConcortHotelPage.passwordKutusu.sendKeys(ConfigReader.getProperty("CHQAValidPassword"));
+        Thread.sleep(2000);
+        qaConcortHotelPage.loginButonu.click();
+    }
+
+}
