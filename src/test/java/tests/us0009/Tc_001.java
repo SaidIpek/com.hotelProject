bcb7d577cada51230ea8aed875cbaa30d587bdf7
SaidIpek <saidipek35@gmail.com>
2021-10-31 23:23:39 +0300

    Said

--- src/test/java/tests/us0009/Tc_001.java
+++ src/test/java/tests/us0009/Tc_001.java
@@ -5,6 +5,6 @@ import org.testng.annotations.Test;
 public class Tc_001 {
     @Test
     public void test(){
-        System.out.println("berrin abla haydi seni beklıyoruz");
+        System.out.println("berrin abla haydi seni beklıyoruz....");
     }
 }
