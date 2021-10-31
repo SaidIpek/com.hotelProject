43f19572b101c472de9f44334521d3625a6fa6f8
SaidIpek <saidipek35@gmail.com>
2021-10-31 23:13:09 +0300

    V9

--- src/test/java/tests/us0009/Tc_001.java
+++ src/test/java/tests/us0009/Tc_001.java
@@ -1,4 +1,10 @@
 package tests.us0009;
 
+import org.testng.annotations.Test;
+
 public class Tc_001 {
+    @Test
+    public void test(){
+        System.out.println("berrin abla haydi seni beklıyoruz");
+    }
 }
