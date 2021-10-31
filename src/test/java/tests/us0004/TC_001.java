d80c3a2f84e5dc832b4b1aef1ac79e9d6b39ee0c
Sümeyra <feza@gmail.com>
2021-10-31 22:10:27 +0100

    abs

--- src/test/java/tests/us0004/TC_001.java
+++ src/test/java/tests/us0004/TC_001.java
@@ -1,8 +1,15 @@
 package tests.us0004;
 
 
+import org.testng.annotations.Test;
 
 public class TC_001 {
+
+    @Test
+
+    public void test (){
+        System.out.println("  abcgi");
+    }
 
 
 }
