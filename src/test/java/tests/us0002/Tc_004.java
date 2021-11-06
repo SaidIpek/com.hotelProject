3ddc0af921d519ff85a95b7d60acdcdd6d0d4e42
Seda <sedadannot@gmail.com>
2021-11-06 16:00:16 +0300

    sedaDeneme

--- src/test/java/tests/us0002/Tc_004.java
+++ src/test/java/tests/us0002/Tc_004.java
@@ -1,4 +1,23 @@
 package tests.us0002;
 
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+import pages.QAConcortPage;
+import utilities.ConfigReader;
+import utilities.Driver;
 public class Tc_004 {
+    //4-Log in butonu gorunur, tiklanabilir olmali ve basarili giris yapilabilmeli
+    @Test
+    public void test() throws InterruptedException {
+        Driver.getDriver().get(ConfigReader.getProperty("CHQAUrl"));
+        QAConcortPage qaConcortPage = new QAConcortPage();
+        qaConcortPage.ilkLoginLinki.click();
+        Thread.sleep(2000);
+        qaConcortPage.usernameKutusu.sendKeys(ConfigReader.getProperty("CHQAValidUsername") + Keys.ENTER);
+        qaConcortPage.passwordKutusu.sendKeys(ConfigReader.getProperty("CHQAValidPassword") + Keys.ENTER);
+        qaConcortPage.loginButonu.isDisplayed();
+        qaConcortPage.loginButonu.click();
+
+        Driver.closeDriver();
+    }
 }
