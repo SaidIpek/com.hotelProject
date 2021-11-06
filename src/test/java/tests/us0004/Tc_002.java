e20a9aa575f51e4d1daf8abe83696531b14671a1
Your Name <you@example.com>
2021-11-06 13:43:53 +0100

    son

--- src/test/java/tests/us0004/Tc_002.java
+++ src/test/java/tests/us0004/Tc_002.java
@@ -1,10 +1,6 @@
 package tests.us0004;
 
-import org.testng.annotations.Test;
-
 public class Tc_002 {
-    @Test
-    public void test() {
-        System.out.println("test 2");
+
     }
-}
+
--- src/test/java/tests/us0004/Tc_003.java
+++ src/test/java/tests/us0004/Tc_003.java
@@ -1,10 +1,5 @@
 package tests.us0004;
 
-import org.testng.annotations.Test;
-
 public class Tc_003 {
-    @Test
-    public void test() {
-        System.out.println("test deneme");
-    }
+
 }
