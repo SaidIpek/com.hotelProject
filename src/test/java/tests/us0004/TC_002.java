b6905de1974029f7d2e0dd98929387d2917d1c1c
Sümeyra <feza@gmail.com>
2021-11-06 14:12:27 +0100

    sumeyra1

--- src/test/java/tests/us0004/TC_002.java
+++ src/test/java/tests/us0004/TC_002.java
@@ -1,10 +1,32 @@
 package tests.us0004;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.QAConcortPage;
+import utilities.ConfigReader;
+import utilities.Driver;
 
 public class TC_002 {
     @Test
-    public void test() {
-        System.out.println("test 2");
+    public void test() throws InterruptedException {
+
+        // 1-Acilan sayfada sol menuden "System Management" menusunun altindaki
+        // "Hotel Management" sekmesine  tiklar
+        QAConcortPage qaConcortPage= new QAConcortPage();
+
+        qaConcortPage.ConcortHotelLogin();
+        Thread.sleep(2000);
+
+        qaConcortPage.US04HotelManagementMenüsü.click();
+
+        // 2-Ardindan "Hotel List" secenegine tiklar.
+        qaConcortPage.US04HotelList.click();
+
+        // "LIST OF HOTEL" sayfasinda otel listesinin tablosu gorunmeli
+        Assert.assertTrue(qaConcortPage.US4OtelEklemeListOfHotelsTablosu.isDisplayed());
+
+        Driver.closeDriver();
+
+
     }
 }
