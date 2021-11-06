f52855012661cdb32f602c0d16f2ebded7d633ba
Berrin <berrink.k@gmail.com>
2021-11-06 15:48:40 +0300

    berrin

--- src/test/java/tests/us0003/Tc_001.java
+++ src/test/java/tests/us0003/Tc_001.java
@@ -1,6 +1,17 @@
 package tests.us0003;
 
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QAConcortPage;
+import utilities.ConfigReader;
+import utilities.Driver;
 public class Tc_001 {
-
+    @Test
+    public void test(){
+        Driver.getDriver().get(ConfigReader.getProperty("CHQAUrl"));
+        QAConcortPage concortHotelPage=new QAConcortPage();
+        Assert.assertTrue(concortHotelPage.createAnewAccountbutonu.isDisplayed());
+        Driver.closeDriver();
+    }
 }
