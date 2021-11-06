20485c7a3391f97d3f99548308a7b467169988e4
hulya <hly@gmail.com>
2021-11-06 15:52:05 +0300

    proje5

--- src/test/java/tests/us0005/Tc_002.java
+++ src/test/java/tests/us0005/Tc_002.java
@@ -1,4 +1,51 @@
-package tests.us0005;
 
-public class Tc_002 {
-}
+    package tests.us0005;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QAConcortPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+    public class Tc_002 {
+        /*US-2 -Tabloda bulunan "Details" Butonunun gorulebilir ve tiklanabilir olmasi gerekir, Acilacak olan sayfada ise "Edit Hotel" yazisi gorunmeli,
+        "General Data","Photos","Properties" sekmeleri bulunmali
+         TC-002
+         1-Kullanici Concort Hotel (Admin) sayfasina gider.
+         2-Sag ustte log in butonunu tiklar ardindan Username ve Sifre girilmeli
+         3-Acilan sayfada sol menuden "System Management" menusunun altindaki "Hotel Management" sekmesine  tiklar
+         4-Ardindan "Hotel List" secegine tiklar.
+         5-"LIST OF HOTELS" tablosunda bulunan "DETAILS"  butonu gorulmeli ardindan tiklanmali
+         6-"Edit Hotel" basliginin altinda "General Data", "Photos", "Properties" bolumleri bulunmali
+         ve tiklanabilir olmali
+         7-Acilacak "Edit Hotel" sayfasinda "Hotel Data" bolumu bulunmali
+       */
+        @Test
+        public void test() throws InterruptedException {
+            Driver.getDriver().get(ConfigReader.getProperty("CHQAUrl"));
+            QAConcortPage qaConcortPage = new QAConcortPage();
+            qaConcortPage.ConcortHotelLogin();
+            Actions actions = new Actions(Driver.getDriver());
+            Thread.sleep(5000);
+            qaConcortPage.hotelManagementLinki.click();
+            Thread.sleep(5000);
+            qaConcortPage.hotelListLink.click();
+            Assert.assertTrue( qaConcortPage.ListOfHotelYAziElementi.isDisplayed(),"list hotel yazisi gorunmuyor");
+            Assert.assertTrue( qaConcortPage.ListOfHotelYAziElementi.isDisplayed(),"list hotel yazisi gorunmuyor");
+            Assert.assertTrue(qaConcortPage.detailsButonu.isDisplayed(),"detail yazisi gorunmuyor");
+            qaConcortPage.detailsButonu.click();
+            Thread.sleep(5000);
+            Assert.assertTrue(qaConcortPage.GeneralDataListLinki.isDisplayed(),"general data yazisi bulunmuyor");
+            qaConcortPage.photosListLinki.click();
+            Assert.assertTrue(qaConcortPage.photosListLinki.isDisplayed(),"photos yazisi bulunmuyor");
+            qaConcortPage.photosListLinki.click();
+            Assert.assertTrue(qaConcortPage.PropertiesListLinki.isDisplayed(),"properties yazisi bulunmuyor");
+            qaConcortPage.PropertiesListLinki.click();
+            Assert.assertTrue(qaConcortPage.EditHotelListYAziElementi.isDisplayed(),"Edit Hotel YAzi Elementi bulunmuyor");
+            Assert.assertTrue(qaConcortPage.HotelDataListLinki.isDisplayed()," Hotel Data Yazi Elementi bulunmuyor");
+       Driver.closeDriver();
+        }
+
+
+    }
+
+
--- src/test/java/tests/us0005/Tc_005.java
+++ src/test/java/tests/us0005/Tc_005.java
@@ -1,4 +1,39 @@
 package tests.us0005;
-
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QAConcortPage;
+import utilities.ConfigReader;
+import utilities.Driver;
 public class Tc_005 {
+    //TC-0005- "General Data" kisminda "Delete Hotel" yazisinin gorulebilir ve "Delete"  butonu tiklanilabilir olmali
+    //ardindan"Hotel successfully deleted" yazisi gorunmeli ve "ok" butonuna tiklanilabilmeli
+    //6-Tekrar "DETAILS" kisminda "General Data" bolumune gidilir
+    //7-"Delete Hotel" Kisminda "DELETE" butonu gorunur olmali
+    //8-"DELETE' butonuna tiklanilmali
+    //9-"Hotel successfully deleted" yazisi gorunmeli ve ardindan "OK" butonuna basilmali
+    @Test
+    public void test() throws InterruptedException {
+        Driver.getDriver().get(ConfigReader.getProperty("CHQAUrl"));
+        QAConcortPage qaConcortPage = new QAConcortPage();
+        qaConcortPage.ConcortHotelLogin();
+        Actions actions = new Actions(Driver.getDriver());
+        Thread.sleep(5000);
+        qaConcortPage.hotelManagementLinki.click();
+        Thread.sleep(5000);
+        qaConcortPage.hotelListLink.click();
+        Assert.assertTrue( qaConcortPage.ListOfHotelYAziElementi.isDisplayed(),"list hotel yazisi gorunmuyor");
+        actions.sendKeys(qaConcortPage.detailsListButonu)
+                .sendKeys(qaConcortPage.GeneralDataListLinki)
+                .sendKeys(qaConcortPage.deleteListIkonu)
+                .sendKeys(qaConcortPage.deleteListButonu)
+                .perform();
+        Assert.assertTrue(qaConcortPage.deleteListButonu.isDisplayed());
+        qaConcortPage.deleteListButonu.click();
+        // Assert.assertTrue();
+        //Actions actions1 = new Actions(Driver.getDriver());
+        // actions.contextClick().perform();
+        Driver.closeDriver();
+    }
 }
