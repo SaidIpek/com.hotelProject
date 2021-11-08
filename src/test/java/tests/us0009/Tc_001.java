5d60e8b8174aa7bc68e4836bb1e851248dba49a2
SaidIpek <saidipek35@gmail.com>
2021-11-08 14:50:26 +0300

    Said09

--- src/test/java/tests/us0009/Tc_001.java
+++ src/test/java/tests/us0009/Tc_001.java
@@ -1,8 +1,24 @@
 package tests.us0009;
 
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QAConcortPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class Tc_001 {
+    @Test
+    public void test() {
 
-
-
-
+        QAConcortPage qaConcortPage = new QAConcortPage();
+        qaConcortPage.ConcortHotelLogin();
+        qaConcortPage.hotelManagementSekmesi.click();
+        Assert.assertTrue(qaConcortPage.roomReservationsSekmesi.isDisplayed());
+        Assert.assertTrue(qaConcortPage.roomReservationsSekmesi.isEnabled());
+        qaConcortPage.roomReservationsSekmesi.click();
+        Assert.assertTrue(qaConcortPage.listOfReservationYazısı.isDisplayed());
+        Driver.closeDriver();
+    }
 }
